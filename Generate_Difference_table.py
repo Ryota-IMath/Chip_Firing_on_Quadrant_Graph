d7ca08c6a63812d96ccbef04fc96ce31d5e0d7ae
Ryota-IMath <inaga270@mit.edu>
2025-12-06 18:13:59 -0500

    Create Generate_Difference_table.py

---
+++ Generate_Difference_table.py
@@ -0,0 +1,23 @@
+def difference_table(n):
+    l1 = [2**n]
+    stab = False
+    while not stab:
+        l2 = [l1[0]//2]
+        for i in range(1, len(l1)):
+            l2.append(l1[i]//2 + l1[i-1]//2)
+            l2.append(l1[len(l1)-1]//2)
+        l1 = l2
+        s1 = set(l1)
+        if not (s1-set([1, 0])):
+            stab = True
+        if l1[0] == 0:
+            l1 = l1[1:]
+        if l1[-1] == 0:
+            l1 = l1[:len(l1)-1]
+        l5 = [abs(t - s) for s, t in zip(l1, l1[1:])]
+        str_out = ""
+        for s in l5:
+            str_out = str_out + str(s) +" "
+        print(str_out)
+        row_count+= 1
+difference_table(9)
