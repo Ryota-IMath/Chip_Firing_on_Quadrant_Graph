0f0245496005317137fd47db2389c5df3687f5a7
Ryota-IMath <inaga270@mit.edu>
2025-12-30 15:58:35 -0500

    Update Generate_Difference_table.py

--- Generate_Difference_table.py
+++ Generate_Difference_table.py
@@ -6,7 +6,7 @@ def difference_table(n):
         l2 = [l1[0]//2]
         for i in range(1, len(l1)):
             l2.append(l1[i]//2 + l1[i-1]//2)
-            l2.append(l1[len(l1)-1]//2)
+        l2.append(l1[len(l1)-1]//2)
         l1 = l2
         s1 = set(l1)
         if not (s1-set([1, 0])):
@@ -20,5 +20,4 @@ def difference_table(n):
         for s in l5:
             str_out = str_out + str(s) +" "
         print(str_out)
-        row_count+= 1
 difference_table(9)
